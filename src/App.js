d298acd04f04b9f94e2e447e26811ce7e7dce5b2
Arebiter <pasandharmasena@gmail.com>
2022-02-01 14:46:30 -0500

    Something went wrong with the project, make initial commit again

--- src/App.js
+++ src/App.js
@@ -2,22 +2,44 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  /*
+  Create a Travel checklist application that meets the specified requirements.
+  ● Must be able to add items
+  ● Items must have a name, quantity, and state (packed/unpacked)
+  ● Must be able to mark and unmark items as packed
+  
+  Will use react hooks
+  -useState for creating items
+  
+  -page will have area to create a form
+    -need input for name
+    -number input for amount
+    -button or check for state
+  
+    each item made will be added to items array
+  
+  -page will have area displaying list of items
+    -list will be made by mapping through items array
+    -each item will have button for checking state
+    -each item will have delete button
+    -maybe add an edit button? 
+  
+    
+  
+  */
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Travel Checklist</h1>
+      <div className='checklist-main'>
+        <div className='item-form'>
+          <p>add an item</p>
+        </div>
+        <div className='item-list'>
+          <p>item list</p>
+        </div>
+      </div>
     </div>
   );
 }
