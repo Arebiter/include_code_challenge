06aa51f5e755960cfd750bf20ec93c120e606b9c
Arebiter <pasandharmasena@gmail.com>
2022-02-01 17:20:15 -0500

    Set up basic form for creating items

--- src/components/itemForm.js
+++ src/components/itemForm.js
@@ -2,9 +2,21 @@ import React from "react";
 
 
 function ItemForm() {
+    //function to handle input
+
+
     return (
         <div className="item-form">
-            <p>add an item</p>
+            <p>Add Item</p>
+            <form className="item-create-form">
+                <input type="text" className="item-name" />
+                <input type="number" className="item-quantity" />
+                <select name="status" className="item-status">
+                    <option value="unpacked">Unpacked</option>
+                    <option value="packed">Packed</option>
+                </select>
+                <input type="submit" className="item-create-button" value="Add Item" />
+            </form>
         </div>
     )
 }
