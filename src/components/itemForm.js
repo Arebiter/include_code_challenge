8572273feed9858c65b0699f1fd843db4f7536fd
Arebiter <pasandharmasena@gmail.com>
2022-02-03 14:30:54 -0500

    Remove handleName and handleQuantity for handleChange to handle changes for all parts of itemInfo

--- src/components/itemForm.js
+++ src/components/itemForm.js
@@ -3,16 +3,15 @@ import React from "react";
 
 const ItemForm = ({ itemInfo, setItemInfo }) => {
     //functions to handle input
-    const handleName = (e) => { //handle name changes only the name information in the item info
-        const info = { ...itemInfo };
-        info.name = e.target.value;
-        setItemInfo(info);
-    }
-
-    const handleQuantity = (e) => {
-        const info = { ...itemInfo };
-        info.quantity = e.target.value;
-        setItemInfo(info);
+    const handleChange = (e) => { //handle name changes only the name information in the item info
+        // const info = { ...itemInfo };
+        // info.name = e.target.value;
+        // setItemInfo(info);
+        setItemInfo({
+            ...itemInfo,
+            [e.target.name]: e.target.value
+        });
+        console.log(itemInfo)
     }
 
 
@@ -20,11 +19,11 @@ const ItemForm = ({ itemInfo, setItemInfo }) => {
         <div className="item-form">
             <p>Add Item</p>
             <form className="item-create-form">
-                <input type="text" className="item-name" onChange={handleName} />
-                <input type="number" className="item-quantity" onChange={handleQuantity} />
-                <select name="status" className="item-status">
-                    <option value="unpacked">Unpacked</option>
-                    <option value="packed">Packed</option>
+                <input type="text" className="item-name" onChange={handleChange} value={itemInfo.name} name="name" />
+                <input type="number" className="item-quantity" onChange={handleChange} value={itemInfo.quantity} name="quantity" />
+                <select name="status" className="item-status" onChange={handleChange} value={itemInfo.packed} name="packed">
+                    <option value={false}>Unpacked</option>
+                    <option value={true}>Packed</option>
                 </select>
                 <input type="submit" className="item-create-button" value="Add Item" />
             </form>
