9c22867c79c4ac6fa2a8321c4d2333b7cb6f660a
Arebiter <pasandharmasena@gmail.com>
2022-02-04 12:34:52 -0500

    Finish packItem function to change the packed status of items. Also added ternary to item className to allow for visual changes through CSS

--- src/components/item.js
+++ src/components/item.js
@@ -5,28 +5,27 @@ const Item = ({ item, setItems, items }) => {
     const { name, quantity, packed } = item;
     // const [packed, setPacked] = useState(false);
 
-    // const handlePacked = () => {
-    //     if (packed) {
-    //         setPacked(false);
-    //     } else {
-    //         setPacked(true);
-    //     }
-    //     console.log(packed);
-    // }
-
     const deleteItem = (e) => {
         setItems(items.filter((ele) => ele.id !== item.id));
     }
 
     const packItem = (e) => {
-
+        setItems(items.map((ele) => { //go over the items
+            //find the item you've clicked on based on the id in the items list
+            if (ele.id === item.id) {
+                return { //change the packed status of the item in the items list
+                    ...ele, packed: !ele.packed
+                }
+            }
+            return ele; //return the ele if you don't find it in the list
+        }))
     }
-
+    //added ternary to item className to add "complete" if the item's packed state is true 
     return (
-        <div className="item">
+        <div className={`item ${item.packed ? ("complete") : ("")}`}>
             <p className="item-name">{name}</p>
             <p className="item-quantity">{quantity}</p>
-            <button className="item-packed">Packed</button>
+            <button className="item-packed" onClick={packItem}>Packed</button>
             <button className="item-delete" onClick={deleteItem}>Delete</button>
         </div>
     )
